6e3b566eed9100939b91b96b946137f8a2d3fdf2
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-06 13:43:38 -0700

    added print alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry 
- * Return: Always 0 (Success
+ * main - Entry point
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
 	char c;
 
 	c = 'a';
-	while 
+	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
