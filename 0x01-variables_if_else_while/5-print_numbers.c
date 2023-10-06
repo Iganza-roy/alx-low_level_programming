a9366af2f4990dc8ace60a5568075421226361e3
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-06 15:01:15 -0700

    added 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Prints numbers from 0 to 9.
  *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
 	putchar('\n');
 	return (0);
+}
