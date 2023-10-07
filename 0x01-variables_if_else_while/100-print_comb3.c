97880b0e104ac4eff45d3cd6dea4d9fea0757d41
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-07 08:07:00 -0700

    added 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,23 @@
 int main(void)
 {
 	int n, m;
-	
-	for (n = 48; m <= 56; n++)
+
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (m = 49; m <= 58; m++)
 		{
 			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if (n != 56 || m != 57)
+				if (n != 56 || m != 58)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-		}
+
+			}
 	}
 	putchar('\n');
 	return (0);
