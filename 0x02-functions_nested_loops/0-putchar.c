0afda6874f71bc05154e287136c9c5e855327c31
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-11 06:47:14 -0700

    added putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Te character to print 
+ * Return : on success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains my projects on functions and nested loops in c programming
