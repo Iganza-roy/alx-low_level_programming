d8a305198d1da63531316b7ea20f2cfa08693df8
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-19 14:37:26 -0700

    added strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int k;
+
+	for (k = 0; s1[k] != '\0' || s2[k] != '\0'; k++)
+	{
+		if (s1[k] != s2[k])
+		{
+			if (s1[k] < s2[k])
+				return (s1[k] - s2[k]);
+			else if (s2[k] < s1[k])
+				return (s1[k] - s2[k]);
+		}
+	}
+	return (0);
+}
