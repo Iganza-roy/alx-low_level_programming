c7b5883f44ba101f6f2aa1616c3761204da3a703
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-23 11:50:29 -0700

    added strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches for a string
+ * @s: string1
+ * @accept: string2
+ *
+ * Return: pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		const char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			return (s);
+			a++;
+		}
+	}
+	return (NULL);
+}
