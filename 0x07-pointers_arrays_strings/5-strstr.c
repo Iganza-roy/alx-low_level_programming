a79a20703f0386490b631e33d6f41c1ec0b7f6b6
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-23 12:35:35 -0700

    added print_diagsums

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string1
+ * @needle: substring
+ *
+ * Return: pointer to the beginning of the substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
