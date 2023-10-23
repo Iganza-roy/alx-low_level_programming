faba3e738661b9584f152e04f3ebc85b7bdb05ee
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-23 12:51:41 -0700

    added print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: an array
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+			_putchar(a[i][k]);
+		_putchar('\n');
+	}
+}
