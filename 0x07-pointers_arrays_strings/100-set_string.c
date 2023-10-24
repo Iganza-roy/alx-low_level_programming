7cca5dec14fee833432cd38877919c63707e64a6
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-24 07:49:15 -0700

    added set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a poinrt to a char
+ * @s: pointer to a pointer
+ * @to: charcter pointer
+ *
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
