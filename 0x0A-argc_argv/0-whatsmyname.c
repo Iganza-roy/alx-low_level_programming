44b6d043354a02af8dd5466091af81ab717ebe9c
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-10-27 23:39:44 -0700

    added whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - prints its name
  * @argc: argument count
  * @argv: argument vector
- * Return 0;
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
@@ -12,5 +12,5 @@ int main(int argc, char *argv[])
 	(void) argc;
 
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
