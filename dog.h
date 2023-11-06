3029ffc5945c49681f7034b334366404d8de0218
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-11-06 12:33:10 -0800

    added

---
+++ dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - has 3 elements
+ * @name: element 1
+ * @age: element 2
+ * @owner: element 3
+ *
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
