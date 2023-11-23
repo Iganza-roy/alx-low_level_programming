a3a6149b13b615f6205a7bd427fe3a8819cd5087
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-11-23 12:51:22 -0800

    added 1-get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,14 +10,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int k;
-	
+
 	if (n == 0 && index < 64)
 		return (0);
 	for (k = 0; k < 63; n >>= 1, k++)
 	{
 		if (index == k)
 		{
-			return(n & 1);
+			return (n & 1);
 		}
 	}
 	return (-1);
