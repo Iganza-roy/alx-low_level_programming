78ed24a572799a8f20d5d206c20ea8701fbb3a09
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-02 04:21:43 -0800

    added 1-djb2.c and 1-main.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,6 +32,8 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size); /*creating hash_table*/
+unsigned long int hash_djb2(const unsigned char *str); /*implementing the djb2 algorithm*/
+
 
 #endif
