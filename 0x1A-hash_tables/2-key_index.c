4ce92762e03c40e21d87c8a32253ae3a3bd02ca2
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-02 04:37:02 -0800

    fixed betty doc

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,12 @@
 #include "hash_tables.h"
+
+/**
+ * key_index - function that returns the index of a key
+ * @key: the key
+ * @size: the size of the array of the hash table
+ * Return: index at which the key/value pair should be stored in the array
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_val;
