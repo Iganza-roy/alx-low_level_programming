882d086f95a4a12002d1e4482731743fa1bbb302
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-20 10:50:56 -0700

    fixed codeing style

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,14 +4,15 @@
 
 def island_perimeter(grid):
     """
-    defining the island_perimeter funtion that \ 
-    returns the perimeter of the island described in grid:
+    defining the island_perimeter funtion that \
+            returns the perimeter of the island described in grid:
 
     Args:
     grid (list): 0 represents a water zone
                 1 represents a land zone
                 One cell is a square with side length 1
-                Grid cells are connected horizontally/vertically (not diagonally).
+                Grid cells are connected horizontally/vertically \
+                        (not diagonally).
                 Grid is rectangular, width and height don’t exceed 100
     """
     perim = 0
@@ -24,4 +25,4 @@ def island_perimeter(grid):
                     perim -= 2
                 if (k > 0 and grid[i][k - 1] == 1):
                     perim -= 2
-    return perim 
+    return perim
